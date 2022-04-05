2504d717e074623a9caa2ffd5df8ebb47304375f
ramprasadsuthi <59646329+ramprasadsuthi@users.noreply.github.com>
2022-04-05 10:19:38 +0530

    Update LargestOfTwo.java

--- src/basics/LargestOfTwo.java
+++ src/basics/LargestOfTwo.java
@@ -10,8 +10,10 @@ public class LargestOfTwo {
 		
 		if(a > b) {
 			System.out.println("A is the largest : " + a);
-		} else {
+		} else if(b > a) {
 			System.out.println("B is the largest : " + b);
+		} else {
+			System.out.println("Both A & B are equal : " + a , " - " b);
 		}
 		
 	}
